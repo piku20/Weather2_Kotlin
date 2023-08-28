5dd2d796ed29b4dd62d375b6eb0becf2576f7242
piku <pikuv89@gmail.com>
2023-08-28 19:35:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/weather2/ui/MainActivity.kt
@@ -0,0 +1,143 @@
+package com.example.weather2.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import com.android.volley.ClientError
+import com.android.volley.NetworkResponse
+import com.android.volley.Request
+import com.android.volley.ServerError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weather2.R
+import com.example.weather2.databinding.ActivityMainBinding
+import com.example.weather2.utils.TOKEN
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import kotlin.math.ceil
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //noLimit
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+        //initViews
+        binding.apply{
+            btnSearch.setOnClickListener{
+                val location = etLocation.text.toString().trim()
+                if(location.isNotEmpty()){
+                    //hide error message
+                    showError(false)
+                    //reques to server
+                    requestToServer(location)
+                }else{
+                    showError(true,"Enter a location")
+                }
+            }
+        }
+    }
+
+    private fun requestToServer(location: String){
+        //volley
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$location&units=metric&appid=$TOKEN"
+        val stringRequest = StringRequest(Request.Method.GET, url, { response ->
+            //Handle API response
+            try{
+                val jsonResponse = JSONObject(response)
+                val mainObject: JSONObject = jsonResponse.getJSONObject("main")
+                val temperature = mainObject.getDouble("temp")
+                val humidity = mainObject.getDouble("humidity")
+                val windObject: JSONObject = jsonResponse.getJSONObject("wind")
+                val windSpeed = windObject.getDouble("speed")
+                val weatherArray: JSONArray = jsonResponse.getJSONArray("weather")
+                var description : String?=null
+                var main:String?=null
+                if (weatherArray.length() > 0)
+                {
+                    val weatherObject = weatherArray.getJSONObject(0)
+                    description = weatherObject.getString("description")
+                    main = weatherObject.getString("main")
+                }
+
+                binding.layoutWeather.apply{
+                    root.visibility = View.VISIBLE
+                    //setData
+                    locationTitle.text = location
+                    val roundValue = ceil(temperature).toInt().toString()
+                    tvTemperature.text = roundValue
+
+                    tvWindSpeed.text = "$windSpeed m/s"
+                    tvHumidityValue.text = "$humidity%"
+                    tvDescription.text = description.toString()
+
+                    when(main){
+                        "Clear"->{
+                            ivStatus.setImageResource(R.drawable.clear)
+                        }
+                        "Rain"->{
+                            ivStatus.setImageResource(R.drawable.rain)
+                        }
+                        "Snow"->{
+                            ivStatus.setImageResource(R.drawable.snow)
+                        }
+                        "Clouds"->{
+                            ivStatus.setImageResource(R.drawable.cloud)
+                        }
+                    }
+
+                    //click Listener
+                    ivBack.setOnClickListener{
+                        root.visibility = View.GONE
+                    }
+                }
+            }catch (e:JSONException){
+                Toast.makeText(
+                    this@MainActivity,
+                    e.message.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        },{
+            if(it is ClientError || it is ServerError){
+                val response : NetworkResponse = it.networkResponse
+                val statusCode = response.statusCode
+                if(statusCode == 404){
+                    showError(true, "Location Not Found")
+                }else{
+                    Toast.makeText(
+                        this,
+                        "error in connection to api",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
+        queue.add(stringRequest)
+    }
+
+    private fun showError(
+        haveError:Boolean,
+        message:String ?= null
+    ){
+        binding.tvError.apply {
+            if(haveError){
+                text = message
+                visibility = View.VISIBLE
+            }else{
+                visibility = View.GONE
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weather2/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.weather2.utils
+
+const val TOKEN = "4a0721278942e9902d53a5806a0e5a14"
